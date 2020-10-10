07b8c0e4e55e8f5a2b31dcc7a2fbf757c8360cae
Harinath120 <konepalli.009@gmail.com>
2020-10-10 20:28:58 +0530

    IAM iser creation in terraform

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -14,3 +14,11 @@ resource "aws_s3_bucket" "my_s3_bucket" {
 resource "aws_iam_user" "my_iam_user" {
     name = "aws_iam_user.hari"
 }
+
+output "my_s3_bucket_versioning" {
+    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
+
+output "my_iam_user_complete_details" {
+    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
