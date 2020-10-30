0e18287649daaf39da58d8f654730bcd5ad62a36
Harinath120 <konepalli.009@gmail.com>
2020-10-30 16:47:00 +0530

    terraform ec2 instance creation

---
+++ 03-terraform-Ec2-creation/ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "AWSServer" {
+    ami = "ami-0ad289a92ed067259"
+    instance_type = "t2.micro"
+    key_name = "devops"
+    security_groups = "launch-wizard-1"
+    tags = {
+        Name = "Terraform Server - Hari"
+    }
+}
