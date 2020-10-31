23b47b25c48a73f10e22bb21ee3127613010b8d0
Harinath120 <konepalli.009@gmail.com>
2020-10-31 08:22:11 +0530

    modified configuration

--- 03-terraform-Ec2-creation/ec2.tf
+++ 03-terraform-Ec2-creation/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "AWSServer" {
-    ami = "ami-0ad289a92ed067259"
+    ami = "ami-052c08d70def0ac62"
     instance_type = "t2.micro"
     key_name = "devops"
     security_groups = ["launch-wizard-1"]
