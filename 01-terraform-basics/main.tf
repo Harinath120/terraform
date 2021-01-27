8238ccd54ef4e248483b1b08f14567a8edde0de0
Harinath120 <konepalli.009@gmail.com>
2021-01-27 17:31:53 +0530

    IAM user creation with output

--- 01-terraform-basics/main.tf
+++ 01-terraform-basics/main.tf
@@ -20,3 +20,7 @@ output "my_s3_bucket_versioning" {
 output "my_s3_bucket_complete_details" {
     value = aws_s3_bucket.my_s3_bucket 
 }
+
+output "my_iam_user_complete_details" {
+    value = aws_iam_user. my_iam_user
+}
