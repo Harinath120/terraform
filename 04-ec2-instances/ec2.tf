318cc4164f7614d241d450618b213644132529b6
Harinath120 <konepalli.009@gmail.com>
2021-01-28 19:03:51 +0530

    creating Ec2 instances

--- 04-ec2-instances/ec2.tf
+++ 04-ec2-instances/ec2.tf
@@ -15,6 +15,20 @@ data "aws_subnet_ids" "default_subnets" {
     vpc_id = aws_default_vpc.default.id 
 }
 
+data "aws_ami" "aws-linux-2-latest" {
+    most_recent = true 
+    owners      = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*"]
+    }
+
+}
+
+data "aws_ami_ids" "aws_linux_2_latest_ids" {
+    owners     = ["amazon"]
+}
+
 //HTTP server -> SG 
 // SG -> 80 TCP, 22 TCP,CIDR Block ["0.0.0.0/0"]
 
