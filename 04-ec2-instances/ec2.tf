f52d8911a2d25065ecd1216ee6fb54e2f6d62a65
Harinath120 <konepalli.009@gmail.com>
2021-01-28 12:26:51 +0530

    creating Ec2 instances

--- 04-ec2-instances/ec2.tf
+++ 04-ec2-instances/ec2.tf
@@ -36,3 +36,11 @@ resource "aws_security_group" "http_server_sg" {
 
 
 }
+
+resource "aws_instance" "http_server" {
+    ami = "ami-047a51fa27710816e"
+    key_name = "devops-hari.pem"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+    subnet_id = "subnet-b0c67cef"
+}
