c0a30536b8bef58b41505e65e248d24eff48ab82
Harinath120 <konepalli.009@gmail.com>
2021-01-28 08:17:58 +0530

    creating lists and maps

--- 03-terraform-list-maps/main.tf
+++ 03-terraform-list-maps/main.tf
@@ -1,5 +1,5 @@
 variable "names" {
-    default = ["Hari", "tom", "theertha"]
+    default = ["krishna", "Hari", "tom", "theertha"]
 }
 
 provider "aws" {
@@ -7,6 +7,8 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "my_iam_users" {
-    count = length(var.names)
-    name = var.names[count.index]
+    #count = length(var.names)
+    #name = var.names[count.index]
+    for_each = toset(var.names)
+    name = each.value
 }
